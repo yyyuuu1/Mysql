4cf7b96a888ac7719b192da3e36579acae00b19c
yyyuuu1 <3054029490@qq.com>
2023-02-06 17:37:40 +0800

    createtable.sql

---
+++ createtable.sql
@@ -0,0 +1,21 @@
+#创建表的课堂练习
+-- 字段	属性
+-- Id	整形
+-- name	字符型
+-- sex	字符型
+-- brithday	日期型（date）
+-- entry_date	日期型   (date)
+-- job	字符型
+-- Salary	小数型
+-- resume	文本型
+
+CREATE TABLE emp (id INT , `name` VARCHAR(32),sex CHAR(1),birthday DATE ,
+			entry_date DATETIME, job VARCHAR(32),Salary DOUBLE ,`resume` TEXT)
+			CHARSET utf8 COLLATE utf8_bin ENGINE INNODB;
+			
+-- 添加一条
+INSERT INTO `emp`
+	VALUES(100, '小妖怪', '男', '2000-11-11', 
+		'2010-11-10 11:11:11', '巡山的', 3000, '大王叫我来巡山');
+		
+SELECT * FROM `emp`;
